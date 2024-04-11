13db9822437cea93d8a0e874281b4beb64849013
jinheelee04 <2pro.jlee@gmail.com>
2024-04-11 20:44:39 +0900

    엔티티 클래스 개발2

--- src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package com.jpabook.jpashop.domain.item;
 
+import com.jpabook.jpashop.domain.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한 테이블의 모든 상속된 변수들 컬럼 생성
@@ -17,4 +21,6 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
