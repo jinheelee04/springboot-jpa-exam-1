609d2c399962db2d0bc2721a63ce27442d378148
jinheelee04 <2pro.jlee@gmail.com>
2024-04-12 12:37:57 +0900

    회원 서비스 개발

---
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,58 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return id
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw  new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * @return members
+     */
+    public List<Member> findAll(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     * @param id
+     * @return member
+     */
+    public Member findOne(Long id){
+        return memberRepository.findOne(id);
+    }
+}
