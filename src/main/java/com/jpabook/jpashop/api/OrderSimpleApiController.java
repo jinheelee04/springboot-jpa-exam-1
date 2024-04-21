4cfcb9ca62239f46e6b5100e3170c3ffeb0dac90
jinheelee04 <2pro.jlee@gmail.com>
2024-04-21 14:03:43 +0900

    간단한 주문 조회V2: 엔티티를 DTO로 변환

--- src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package com.jpabook.jpashop.api;
 
+import com.jpabook.jpashop.domain.Address;
 import com.jpabook.jpashop.domain.Order;
 import com.jpabook.jpashop.domain.OrderSearch;
+import com.jpabook.jpashop.domain.OrderStatus;
 import com.jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -34,5 +41,29 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // Lazy 강제 초기화
         }
 ;       return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        // N+1 문제 발생함
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(toList());
+    }
+    @Data
+    private class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화 (영속성 컨텍스트를 조회 후 없으면 쿼리 실행)
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
--- src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/OrderRepository.java
@@ -37,9 +37,11 @@ public class OrderRepository {
     }
 
     // 동적쿼리 예제(실무에서 사용 안함, 버그 발생 가능성 높음)
-    public List<Order> findAllByString(OrderSearch orderSearch){
-        String jpql = "select o From Order o join o.member m";
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
         boolean isFirstCondition = true;
+
         //주문 상태 검색
         if (orderSearch.getOrderStatus() != null) {
             if (isFirstCondition) {
@@ -48,27 +50,30 @@ public class OrderRepository {
             } else {
                 jpql += " and";
             }
-            jpql+=" o.status = :status";
+            jpql += " o.status = :status";
         }
+
         //회원 이름 검색
         if (StringUtils.hasText(orderSearch.getMemberName())) {
             if (isFirstCondition) {
-                jpql+= " where";
+                jpql += " where";
                 isFirstCondition = false;
-            }
-            else {
-                jpql+= " and";
+            } else {
+                jpql += " and";
             }
             jpql += " m.name like :name";
         }
+
         TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                                    .setMaxResults(1000);
+                .setMaxResults(1000);
+
         if (orderSearch.getOrderStatus() != null) {
             query = query.setParameter("status", orderSearch.getOrderStatus());
         }
         if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
         }
-        query = query.setParameter("name", orderSearch.getMemberName());
+
         return query.getResultList();
     }
 
