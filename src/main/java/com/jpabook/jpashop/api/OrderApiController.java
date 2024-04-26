abd980144a2fa84a7076b052d5e1de22e824d31f
jinheelee04 <2pro.jlee@gmail.com>
2024-04-26 15:04:40 +0900

    OSIV 설정 추가

--- src/main/java/com/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -18,6 +18,16 @@ import java.util.List;
 
 import static java.util.stream.Collectors.*;
 
+/**
+ * 권장 순서
+ * 1. 엔티티 조회 방식으로 우선 접근
+ *    1-1. 페치 조인으로 쿼리 수를 최적화
+ *    1-2. 컬렉션 최적화
+ *      1-2-1. 페이징 필요 : hibername.default_batch_fetch_size, @BatchSize로 최적화
+ *      1-2-2. 페이징 필요 X: 페치 조인 사용
+ * 2. 엔티티 조회 방식으로 해결이 안되면 DTO 조회 방식 사용
+ * 3. DTO조회 방식으로 해결 안되면 NativeSQL or 스프링 JdbcTemplate
+ */
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
